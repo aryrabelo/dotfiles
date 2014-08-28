6665886dc942e86c608ce74a7066b3e06a16f442
Ary Rabelo <aryrabelo@gmail.com>
2014-08-28 08:53:48 -0300

    Update bootstrap.sh

--- topics/bin/bootstrap.sh
+++ topics/bin/bootstrap.sh
@@ -39,11 +39,6 @@ if [[ ! -x /usr/local/bin/python ]]; then
     brew install python --framework --with-brewed-openssl
 fi
 
-# Download and install hg
-if [[ ! -x /usr/local/bin/hg ]]; then
-    pip install mercurial
-fi
-
 # Download and install Ansible
 if [[ ! -x /usr/local/bin/ansible ]]; then
     brew install ansible
@@ -52,19 +47,6 @@ fi
 # Make the code directory
 mkdir -p $SRC_DIRECTORY
 
-# Clone down ansible
-if [[ ! -d $ANSIBLE_DIRECTORY ]]; then
-    git clone -b devel git://github.com/danieljaouen/ansible.git $ANSIBLE_DIRECTORY
-fi
-
-# Use the forked Ansible
-source $ANSIBLE_DIRECTORY/hacking/env-setup > /dev/null
-
-# Clone down the Ansible repo
-if [[ ! -d $ANSIBLE_CONFIGURATION_DIRECTORY ]]; then
-    git clone git@bitbucket.org:danieljaouen/ansible-base-box.git $ANSIBLE_CONFIGURATION_DIRECTORY
-    (cd $ANSIBLE_CONFIGURATION_DIRECTORY && git submodule init && git submodule update)
-fi
 
 # Provision the box
 ansible-playbook --ask-sudo-pass -i $ANSIBLE_CONFIGURATION_DIRECTORY/inventories/osx $ANSIBLE_CONFIGURATION_DIRECTORY/site.yml --connection=local
